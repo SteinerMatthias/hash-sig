14525c246476ea115c928710e5867d1107bd6ef8
Matthias Steiner <86098413+SteinerMatthias@users.noreply.github.com>
2025-09-11 15:25:42 +0200

    Update benchmark.rs

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main};
+use criterion::{criterion_group, criterion_main, Criterion};
 
 mod benchmark_poseidon;
 mod benchmark_poseidon_top_level;
@@ -9,9 +9,12 @@ use benchmark_poseidon_top_level::bench_function_poseidon_top_level;
 use benchmark_sha::bench_function_sha;
 
 criterion_group!(
-    benches,
-    bench_function_poseidon_top_level,
-    bench_function_sha,
-    bench_function_poseidon
+    name = benches;
+    config = Criterion::default()
+            .sample_size(1000)
+            .warm_up_time(std::time::Duration::from_secs(5));
+    targets = bench_function_poseidon_top_level,
+              bench_function_sha,
+              bench_function_poseidon
 );
 criterion_main!(benches);
